ca1fe73f4be5560a55cd72a6bbd294c3a9eee7f6
Aravind698 <aravindsankaran03@gmail.com>
2026-07-31 13:30:20 +0530

    Add environment variable support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 import os
+from dotenv import load_dotenv
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor

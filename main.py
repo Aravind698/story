cf5ce5b51bcebf6b79375496bbd11276fa5b1c17
Aravind698 <aravindsankaran03@gmail.com>
2026-07-31 13:25:10 +0530

    updated main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import date
+import os
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
@@ -27,7 +28,8 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
